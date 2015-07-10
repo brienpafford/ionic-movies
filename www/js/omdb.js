50a161a9fb929b43e98d5a0f4167688f65af03c5
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-07-10 18:28:25 -0500

    Gets movie by random IMDB i.d. #. Working.

--- www/js/omdb.js
+++ www/js/omdb.js
@@ -1,18 +1,49 @@
 angular.module('ionicMovies.omdb', [])
 
 .controller('OmdbCtrl', function($scope, $http) {
+  // $scope.search = ''
+         //gets movie by random IMDB I.d.
+      $scope.getMovie = function() {
+           var maximum = 0000000;
+           var minimum = 9999999;
+           var id = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+          $http.get("http://omdbapi.com/?i=tt" + + id)  //
+           .success(function (response) {
+             if(response.Type === "movie"){
+                $scope.name = response.Title;
+                $scope.rating = response.imdbRating;
+                $scope.year = response.Year;
+                $scope.plot = response.Plot;
+              console.log(id);
+              console.log(response)
+            }else{
+              $scope.getMovie();
+            }
+       });
+     }
 
-    $scope.getMovie = function() {
-        // $scope.rndNum();
-        var id = Math.floor((Math.random() * 9999999) + 1);
-        $http.get("http://omdbapi.com/?i=tt" + id)  //
-        .success(function (response) {
-          $scope.name = response.Title;
-          $scope.rating = response.imdbRating;
-          $scope.year = response.Year;
-          $scope.plot = response.Plot;
-        console.log($scope.name);
-        console.log(id);
-    });
-  }
 });
+
+//trying to get this to search by year of release.  CAnt figure out if my input in the search field is getting into the api request
+//    $scope.getMovie = function() {
+//        // var maximum = 2015;
+//        // var minimum = 1900;
+//        // var id = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+//        $http.get("http://omdbapi.com/?i=tt" + search)  // + id
+//         .success(function (response) {
+//           if(response.Type === "movie" && response.Year === $scope.search){
+//              $scope.name = response.Title;
+//              $scope.rating = response.imdbRating;
+//              $scope.year = response.Year;
+//              $scope.plot = response.Plot;
+//            console.log(id);
+//            console.log(response)
+//          }else{
+//            $scope.getMovie();
+//          }
+//     });
+//   }
+
+
+
+
