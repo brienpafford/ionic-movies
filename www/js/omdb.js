58463d1797a02f5d68f47fa5c5e1e7df3d022cfd
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-07-10 15:16:58 -0500

    rando movie gen

--- www/js/omdb.js
+++ www/js/omdb.js
@@ -1,5 +1,17 @@
 angular.module('ionicMovies.omdb', [])
 
-.controller('OmdbCtrl', function($scope) {
+.controller('OmdbCtrl', function($scope, $http) {
 
-})
+    $scope.getMovie = function() {
+        // $scope.rndNum();
+        var id = Math.floor((Math.random() * 9999999) + 1);
+        $http.get("http://omdbapi.com/?i=tt" + id)  //
+        .success(function (response) {
+          $scope.name = response.Title;
+          $scope.rating = response.imdbRating;
+          $scope.plot = response.Plot;
+        console.log($scope.name);
+        console.log(id);
+    });
+  }
+});
