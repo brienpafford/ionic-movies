723cdd1dfff9475f5839399aef657a284f28acc0
Brien Pafford <bcpaff@gmail.com>
2015-07-10 15:20:41 -0500

    Next-flix

--- www/js/roulette.js
+++ www/js/roulette.js
@@ -1,5 +1,23 @@
 angular.module('ionicMovies.roulette', [])
 
-.controller('RouletteCtrl', function($scope) {
+    .controller('RouletteCtrl', function($scope, $http) {
 
-})
+        var API_URL = ("http://netflixroulette.net/api/api.php?");
+
+         $scope.getMovie = function () {
+
+        $http.get('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
+            .success(function(data, status, headers, config) {
+                $scope.title = data.show_title;
+                $scope.year = data.release_year;
+                $scope.rating = data.rating;
+                $scope.category = data.category;
+                $scope.summary = data.summary;
+                $scope.poster = data.poster;
+            })
+            .error(function(data, status, headers, config) {
+
+            });
+
+     }
+});
