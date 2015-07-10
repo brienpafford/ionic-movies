7358654ed189f6fa8b0faf79b3e1d772227dcaa7
Max Trenkle <MaxTrenkle@users.noreply.github.com>
2015-07-10 15:27:30 -0500

    angular stuff

--- www/js/nytr.js
+++ www/js/nytr.js
@@ -1,4 +1,15 @@
 angular.module('ionicMovies.nytr', [])
 
-.controller('NytrCtrl', function($scope) {
-  });
+.controller('NytrCtrl', function($scope, $http) {
+
+	var API_KEY = '3bc8debeaee58804b78f2a23ca726921%3A0%3A72480690';
+
+	$scope.search = '';
+
+	$scope.add = function(search){
+		$http.get("http://crossorigin.me/http://api.nytimes.com/svc/movies/v2/reviews/search.json?query" + search + "&critics-pick=N&api-key=" + API_KEY)
+		.success(function (response) {
+			console.log("response");
+			})
+		}
+});
