f49bb158d9f89208a52c1585d3338d8c04e01711
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-07-11 17:01:17 -0500

    refined code and added tv series

--- www/js/omdb.js
+++ www/js/omdb.js
@@ -4,28 +4,40 @@ angular.module('ionicMovies.omdb', [])
   // $scope.search = ''
          //gets movie by random IMDB I.d. that is rated higher than 7.0
       $scope.getMovie = function() {
-           var maximum = 0000001;
+          $scope.posterAlt = "";
+          $scope.poster = "";
+          $scope.name = "";
+          $scope.cat = "";
+          $scope.actors = "";
+          $scope.rating = "";
+          $scope.year = "";
+          $scope.plot = "";
+          $scope.votes = "";
+           var maximum = 0000000;
            var minimum = 4000000;
            var id = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
           $http.get("http://omdbapi.com/?i=tt" + id)  //
            .success(function (response) {
-             if(response.Type === "movie" && response.imdbRating * 1 > 6  && response.Poster !== "N/A"){
+             if(response.Type === "movie" && response.imdbRating * 1 > 6 || response.Type === "series" && response.imdbRating * 1 > 6){
                 $scope.name = response.Title;
+                $scope.cat = response.Type;
                 $scope.actors = response.Actors;
                 $scope.rating = response.imdbRating;
                 $scope.year = response.Year;
-                $scope.awards = response.Awards;
                 $scope.plot = response.Plot;
                 $scope.votes = response.imdbVotes;
-                $scope.poster = response.Poster;
-
-              console.log(id);
-              console.log(response);
-              console.log($scope.poster);
-            }else{
-              $scope.getMovie();
-            }
-       });
+                if(response.Poster === "N/A"){
+                  $scope.posterAlt = "Sorry.  No poster was available."
+                }else{
+                  $scope.poster = response.Poster;
+                }
+                console.log(id);
+                console.log(response);
+                console.log($scope.poster);
+              }else{
+                $scope.getMovie();
+              }
+        });
      }
 
 });
