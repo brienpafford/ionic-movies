93183e15b856881043db19a918b560cb6385f579
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-07-11 10:14:09 -0500

    refined search process

--- www/js/omdb.js
+++ www/js/omdb.js
@@ -2,20 +2,26 @@ angular.module('ionicMovies.omdb', [])
 
 .controller('OmdbCtrl', function($scope, $http) {
   // $scope.search = ''
-         //gets movie by random IMDB I.d.
+         //gets movie by random IMDB I.d. that is rated higher than 7.0
       $scope.getMovie = function() {
-           var maximum = 0000000;
-           var minimum = 9999999;
+           var maximum = 0000001;
+           var minimum = 4000000;
            var id = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-          $http.get("http://omdbapi.com/?i=tt" + + id)  //
+          $http.get("http://omdbapi.com/?i=tt" + id)  //
            .success(function (response) {
-             if(response.Type === "movie"){
+             if(response.Type === "movie" && response.imdbRating * 1 > 6  && response.Poster !== "N/A"){
                 $scope.name = response.Title;
+                $scope.actors = response.Actors;
                 $scope.rating = response.imdbRating;
                 $scope.year = response.Year;
+                $scope.awards = response.Awards;
                 $scope.plot = response.Plot;
+                $scope.votes = response.imdbVotes;
+                $scope.poster = response.Poster;
+
               console.log(id);
-              console.log(response)
+              console.log(response);
+              console.log($scope.poster);
             }else{
               $scope.getMovie();
             }
